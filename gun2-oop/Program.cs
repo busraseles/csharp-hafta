fa27b79e47b29bc3eb658ec2d0381f6abeff3121
Büşra Seleş <busraseles42@gmail.com>
2026-09-22 11:53:44 +0300

    gun2: 10. adim record ve class karsilastirmasi tamamlandi

--- gun2-oop/Program.cs
+++ gun2-oop/Program.cs
@@ -1,58 +1,17 @@
-﻿ITodoRepository repo = new JsonFileTodoRepository("todos.json");
+﻿var c1 = new NoktaClass { X = 1, Y = 2 };
+var c2 = new NoktaClass { X = 1, Y = 2 };
+var r1 = new NoktaRecord(1, 2);
+var r2 = new NoktaRecord(1, 2);
 
-while (true)
+Console.WriteLine(c1 == c2);
+Console.WriteLine(r1 == r2);
+Console.WriteLine(c1);
+Console.WriteLine(r1);
+
+public class NoktaClass
 {
-    Console.WriteLine();
-    Console.WriteLine("1) Ekle 2) Listele 3) Tamamla 4) Sil 0) Çıkış");
-    Console.Write("Seçim: ");
-    var secim = Console.ReadLine();
-
-    switch (secim)
-    {
-        case "1":
-            Console.Write("Başlık: ");
-            var baslik = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(baslik))
-            {
-                repo.Add(baslik);
-            }
-            break;
-
-        case "2":
-            foreach (var item in repo.GetAll())
-            {
-                var isaret = item.IsDone ? "[x]" : "[ ]";
-                Console.WriteLine($"{item.Id}. {isaret} {item.Title}");
-            }
-            break;
-
-        case "3":
-            Console.Write("Tamamlanacak Id: ");
-            if (int.TryParse(Console.ReadLine(), out int tamamlanacakId))
-            {
-                if (!repo.Complete(tamamlanacakId))
-                    Console.WriteLine("Görev bulunamadı!");
-            }
-            else
-            {
-                Console.WriteLine("Geçersiz Id!");
-            }
-            break;
-
-        case "4":
-            Console.Write("Silinecek Id: ");
-            if (int.TryParse(Console.ReadLine(), out int silinecekId))
-            {
-                if (!repo.Remove(silinecekId))
-                    Console.WriteLine("Görev bulunamadı!");
-            }
-            else
-            {
-                Console.WriteLine("Geçersiz Id!");
-            }
-            break;
-
-        case "0":
-            return;
-    }
+    public int X { get; init; }
+    public int Y { get; init; }
 }
+
+public record NoktaRecord(int X, int Y);
